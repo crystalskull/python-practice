37fda73ce4ec6411f303401d4fd62b70c75f1d7c
Jeevan Kukkalli <jeevank@Jeevans-MacBook-Air.local>
2017-10-15 18:24:22 +0530

    Program to approximate cube root using bisection search
    Signed-off-by: Jeevan Kukkalli <jeevank@Jeevans-MacBook-Air.local>

---
+++ bisection_cube.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+'''
+Program to find cube root of a number using bisection
+search method.
+'''
+
+if __name__ == '__main__':
+  num = int(input("Enter a number:"))
+  epsilon = 0.00001
+  low = min(0, num)
+  high = max(1, num)
+  guess = (low + high) / 2
+  while abs(guess ** 3 - num) > epsilon:
+    print(low, high, guess)
+    if guess ** 3 > num:
+      high = guess
+    else:
+      low = guess
+    guess = (low + high) / 2
+  print("Approximate cube root of {} is {}".format(num, guess))
