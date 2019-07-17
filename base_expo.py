6da93cb1ff09b44fcd403def8387c768014f6894
Jeevan Kukkalli <jeevan1004@gmail.com>
2019-07-17 23:46:03 +0530

    base_expo program optimizations
    Signed-off-by: Jeevan Kukkalli <jeevan1004@gmail.com>

--- base_expo.py
+++ base_expo.py
@@ -7,17 +7,20 @@ Condition: 0 < exponent < 6
 if __name__ == '__main__':
   MAX_EXPO = 6
   num = int(input("Enter a number: "))
-  base = 0
+  base = 1
   if len(str(num)) > 10:
     ans = input("Large integer will take some time to compute. Do you want to proceed?")
   else:
     ans = 'y'
   while ans == 'yes' or ans == 'y':
     expo = 1
-    while expo < MAX_EXPO and base ** expo < abs(num):
-      expo += 1
+    while expo <= MAX_EXPO:
+      if base ** expo >= abs(num):
+        break
+      else:
+        expo += 1
     
-    if expo < MAX_EXPO:
+    if expo <= MAX_EXPO:
         if base ** expo == abs(num):
           if expo % 2 == 0:
             if num < 0:
